580c017454b75dc7f57d29c776f3870ad7ea181d
Guy Wilson <wilson.guy@gmail.com>
2022-06-02 11:08:36 +0100

    Added another test case to test.c

--- test.c
+++ test.c
@@ -4,6 +4,7 @@
 int main(int argc, char ** argv)
 {
     HTXT    ht;
+    HTXT    ht2;
 
     ht = lm_openWithStdArgs(argc, argv);
 
@@ -13,4 +14,14 @@ int main(int argc, char ** argv)
     }
 
     lm_close(ht);
+
+    ht2 = lm_open("email.txt", "email_out.txt");
+
+    while (!lm_isEOF(ht2)) {
+        lm_findDeleteToFound(ht2, "<");
+        lm_findDeleteNum(ht2, "<", 1);
+        lm_findReplace(ht2, ">;", ",\n");
+    }
+
+    lm_close(ht2);
 }
